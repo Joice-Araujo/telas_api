f42a76a581bb7ed9712d2a36aaac3342e57f2a18
Joice-Araujo <112172335+Joice-Araujo@users.noreply.github.com>
2023-09-14 21:35:38 -0300

    feat: add tema claro e provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import {BrowserRouter} from "react-router-dom"
 import { AppRoutes } from "./routes/Routes";
+import { Light } from "@mui/icons-material";
+import { ThemeProvider } from "@mui/material/styles";
+import { LightTheme } from "./shered/themes";
 
 
 
  export const  App = () => {
   return (
+
+    <ThemeProvider theme={LightTheme}>
+
     <BrowserRouter>
     
     <AppRoutes/>
     
     </BrowserRouter>
+     
+    </ThemeProvider>
     
   );
 }
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,4 +1,4 @@
-import { Button, ButtonBase, IconButton } from "@mui/material";
+import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 
@@ -6,7 +6,7 @@ export  const AppRoutes= () =>{
 
     return(
         <Routes>
-            <Route path="/pagina-inicial" element={<Button>Login</Button>}/>
+            <Route path="/pagina-inicial" element={<Button variant="contained" color="primary">Login</Button>}/>
 
             <Route path="*" element={<Navigate to="/pagina-inicial"/>}/>
 
