9d3743d8c2dd6830b6d48c0e69d21d1d94c3f090
Joice-Araujo <112172335+Joice-Araujo@users.noreply.github.com>
2023-09-17 23:54:23 -0300

    titulo na pag meus projetos

--- src/pages/myProjects/MyProjets.tsx
+++ src/pages/myProjects/MyProjets.tsx
@@ -11,9 +11,11 @@ export const MyProjects = () => {
         <Box display="flex" alignItems="center" justifyContent="center" maxHeight="100vh"
         flexDirection="column" sx={{gap:3}}>
 
-        <Paper sx={{mt:3, padding:3, borderRadius: 5,  width: '1000px', height: '480px', gap: 3}}>
+            <Paper sx={{mt:3, padding:3, borderRadius: 5,  width: '1000px', height: '480px', gap: 3}}>
 
-        </Paper>
+                <Typography variant="h4" color="primary">Seus Projetos</Typography>
+
+            </Paper>
 
         </Box>
     )
